8f424691cc5d535fc7c6f5890231fc6a912d21f6
Jul1o21 <julio.cubas@unmsm.edu.pe>
2023-06-16 01:21:38 -0500

    Summary
    Comentarios

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import psycopg2
 app = Flask(__name__)
 
 
-#PÁGINA PRINCIPAL a
+#PÁGINA PRINCIPAL, lista de predios
 @app.route("/")
 def main():
     predios = []
-
-    consulta1 = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio from tipo_predio TP, predio PR where TP.id_tipo_predio = PR.id_tipo_predio;"
+    #Consulta para sacar los predios:
+    consultaPredios = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio from tipo_predio TP, predio PR where TP.id_tipo_predio = PR.id_tipo_predio;"
 
     conn = connection()
     cursor = conn.cursor()
 
     try:
-        cursor.execute(consulta1)
+        cursor.execute(consultaPredios)
         for row in cursor.fetchall():
             predios.append({"id_predio":row[0], "predio":row[1]})
     except psycopg2.Error as error:
@@ -27,24 +27,25 @@ def main():
 
     return render_template("index.html", predios = predios)
 
-
+#Datos Predio seleccionado
 @app.route("/<int:id>")
 def condominio(id):
     predios = []
     prediosi = []
-
-    consulta1 = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio from tipo_predio TP, predio PR where TP.id_tipo_predio = PR.id_tipo_predio;"
-    consulta2 = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio, CONCAT(PE.apellido_paterno, ' ', PE.apellido_materno, ', ', PE.nombres) as presidente from tipo_predio TP, predio PR, persona PE where TP.id_tipo_predio = PR.id_tipo_predio and PE.id_persona = PR.id_persona and PR.id_predio = "+str(id)+";"
+    #Consulta para sacar los predios:
+    consultaPredios = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio from tipo_predio TP, predio PR where TP.id_tipo_predio = PR.id_tipo_predio;"
+    #Consulta para sacar al predio seleccionado y su presidente:
+    consultaPredio_Presidente = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio, CONCAT(PE.apellido_paterno, ' ', PE.apellido_materno, ', ', PE.nombres) as presidente from tipo_predio TP, predio PR, persona PE where TP.id_tipo_predio = PR.id_tipo_predio and PE.id_persona = PR.id_persona and PR.id_predio = "+str(id)+";"
 
     conn = connection()
     cursor = conn.cursor()
 
     try:
-        cursor.execute(consulta1)
+        cursor.execute(consultaPredios)
         for row in cursor.fetchall():
             predios.append({"id_predio":row[0], "predio":row[1]})
 
-        cursor.execute(consulta2)
+        cursor.execute(consultaPredio_Presidente)
         for row in cursor.fetchall():
             prediosi.append({"id_predio":row[0], "predio":row[1], "presidente":row[2]})
     except psycopg2.Error as error:
@@ -55,24 +56,25 @@ def condominio(id):
 
     return render_template("index_CD.html", prediosi = prediosi, predios = predios)
 
-
+#Predio con el cuadro de casas pertenecientes al predio
 @app.route('/<int:id>/cuadroCostos')
 def cuadroCostos(id):
     prediosi = []
     cuadro = []
-
-    consulta1 = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio, CONCAT(PE.apellido_paterno, ' ', PE.apellido_materno, ', ', PE.nombres) as presidente from tipo_predio TP, predio PR, persona PE where TP.id_tipo_predio = PR.id_tipo_predio and PE.id_persona = PR.id_persona and PR.id_predio = "+str(id)+";"
-    consulta2 = "select CA.numero, PM.descripcion as bloque, CE.descripcion as estado, CA.area, (select sum(area) from estacionamiento where id_casa = CA.id_casa) as area_cochera, ((select sum(area) from estacionamiento where id_casa = CA.id_casa) + CA.area) as area_total, 'xxx.xx' as montominimo, 'xxx.xx' as montoxarea from casa CA, predio_mdu PM, casa_estado CE where CA.id_predio = "+str(id)+" and CA.id_predio_mdu = PM.id_predio_mdu and CE.id_estado = CA.id_estado;"
+    #Consulta para sacar al predio seleccionado y su presidente:
+    consultaPredio_Presidente = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio, CONCAT(PE.apellido_paterno, ' ', PE.apellido_materno, ', ', PE.nombres) as presidente from tipo_predio TP, predio PR, persona PE where TP.id_tipo_predio = PR.id_tipo_predio and PE.id_persona = PR.id_persona and PR.id_predio = "+str(id)+";"
+    #Consulta para sacar las abitaciones, con su bloque, con su estado, area, area_cochera, area_total, monto_minimo y monto por area.
+    consultaCuadroHabitaciones = "select CA.numero, PM.descripcion as bloque, CE.descripcion as estado, CA.area, (select sum(area) from estacionamiento where id_casa = CA.id_casa) as area_cochera, ((select sum(area) from estacionamiento where id_casa = CA.id_casa) + CA.area) as area_total, 'xxx.xx' as montominimo, 'xxx.xx' as montoxarea from casa CA, predio_mdu PM, casa_estado CE where CA.id_predio = "+str(id)+" and CA.id_predio_mdu = PM.id_predio_mdu and CE.id_estado = CA.id_estado;"
 
     conn = connection()
     cursor = conn.cursor()
 
     try:
-        cursor.execute(consulta1)
+        cursor.execute(consultaPredio_Presidente)
         for row in cursor.fetchall():
             prediosi.append({"id_predio":row[0], "predio":row[1], "presidente":row[2]})
 
-        cursor.execute(consulta2)
+        cursor.execute(consultaCuadroHabitaciones)
         for row in cursor.fetchall():
             cuadro.append({"numero":row[0], "bloque":row[1], "estado":row[2], "area":row[3], "area_cochera":row[4], "area_total":row[5], "montominimo":row[6], "montoxarea":row[7]})
     except psycopg2.Error as error:
@@ -83,24 +85,25 @@ def cuadroCostos(id):
 
     return render_template("cuadroCostos.html", prediosi = prediosi, cuadro = cuadro)
 
-
+#Predio con el cuadro de casas pertenecientes al predio luego de confirmar los pagos.
 @app.route('/<int:id>/cuadroCostos/descargarRecibo')
 def descargarRecibo(id):
     prediosi = []
     cuadro = []
-
-    consulta1 = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio, CONCAT(PE.apellido_paterno, ' ', PE.apellido_materno, ', ', PE.nombres) as presidente from tipo_predio TP, predio PR, persona PE where TP.id_tipo_predio = PR.id_tipo_predio and PE.id_persona = PR.id_persona and PR.id_predio = "+str(id)+";"
-    consulta2 = "select CA.numero, PM.descripcion as bloque, CE.descripcion as estado, CA.area, (select sum(area) from estacionamiento where id_casa = CA.id_casa) as area_cochera, ((select sum(area) from estacionamiento where id_casa = CA.id_casa) + CA.area) as area_total, 'xxx.xx' as montominimo, 'xxx.xx' as montoxarea from casa CA, predio_mdu PM, casa_estado CE where CA.id_predio = "+str(id)+" and CA.id_predio_mdu = PM.id_predio_mdu and CE.id_estado = CA.id_estado;"
+    #Consulta para sacar al predio seleccionado y su presidente:
+    consultaPredio_Presidente = "select PR.id_predio, CONCAT(TP.nomre_predio, ' \"', PR.descripcion, '\"') as predio, CONCAT(PE.apellido_paterno, ' ', PE.apellido_materno, ', ', PE.nombres) as presidente from tipo_predio TP, predio PR, persona PE where TP.id_tipo_predio = PR.id_tipo_predio and PE.id_persona = PR.id_persona and PR.id_predio = "+str(id)+";"
+    #Consulta para sacar las abitaciones, con su bloque, con su estado, area, area_cochera, area_total, monto_minimo y monto por area.
+    consultaCuadroHabitaciones = "select CA.numero, PM.descripcion as bloque, CE.descripcion as estado, CA.area, (select sum(area) from estacionamiento where id_casa = CA.id_casa) as area_cochera, ((select sum(area) from estacionamiento where id_casa = CA.id_casa) + CA.area) as area_total, 'xxx.xx' as montominimo, 'xxx.xx' as montoxarea from casa CA, predio_mdu PM, casa_estado CE where CA.id_predio = "+str(id)+" and CA.id_predio_mdu = PM.id_predio_mdu and CE.id_estado = CA.id_estado;"
     
     conn = connection()
     cursor = conn.cursor()
     
     try:
-        cursor.execute(consulta1)
+        cursor.execute(consultaPredio_Presidente)
         for row in cursor.fetchall():
             prediosi.append({"id_predio":row[0], "predio":row[1], "presidente":row[2]})
 
-        cursor.execute(consulta2)
+        cursor.execute(consultaCuadroHabitaciones)
         for row in cursor.fetchall():
             cuadro.append({"numero":row[0], "bloque":row[1], "estado":row[2], "area":row[3], "area_cochera":row[4], "area_total":row[5], "montominimo":row[6], "montoxarea":row[7]})
     except psycopg2.Error as error:
@@ -110,7 +113,6 @@ def descargarRecibo(id):
         conn.close()
 
     return render_template("descargarRecibo.html", cuadro = cuadro, prediosi = prediosi)
-
 
 @app.route('/<int:id>/cuadroCostos/descargarRecibo/reciboTotal')
 def reciboTotal(id):
